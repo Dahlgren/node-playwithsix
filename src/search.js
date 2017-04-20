efdfc81ef8b25ed23460a3be555106800966221d
Björn Dahlgren <bjorn@dahlgren.at>
2017-04-20 08:51:59 +0200

    Add additional properties to search results

--- src/search.js
+++ src/search.js
@@ -1,19 +1,12 @@
 var async = require('async');
+var mapModInfo = require('./utils/map_mod_info');
 
 module.exports = function (phrase, mods, cb) {
   phrase = phrase.toLowerCase();
   filteredMods = [];
   mods.forEach(function (mod) {
     if (mod.PackageName.toLowerCase().indexOf(phrase) > -1 || mod.Name.toLowerCase().indexOf(phrase) > -1) {
-      filteredMods.push({
-        dependencies: mod.Dependencies.map(function (dependency) {
-          return dependency.toLowerCase();
-        }),
-        name: mod.PackageName.toLowerCase(),
-        size: mod.SizeWd,
-        title: mod.Name,
-        type: mod.Type,
-      });
+      filteredMods.push(mapModInfo(mod));
     }
   });
   cb(null, filteredMods);
